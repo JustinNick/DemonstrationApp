df0c9fe1c0baf0f5937f3255ace2df3539ff70d7
chengguo <it_chengguo@163.com>
2017-12-13 08:47:01 +0800

    commit first1.

---
+++ app/src/androidTest/java/com/example/it_ch/demonstrationapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.it_ch.demonstrationapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <homepage_fragment href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</homepage_fragment>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
